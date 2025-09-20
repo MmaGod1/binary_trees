015258ea400bd46cab7b386a4a1426f12e1a732d
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-09-20 13:32:07 +0100

    Update 133-heap_extract.c

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,6 +1,21 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_size - measures the size of a binary tree
+ * @tree: pointer to the root node
+ *
+ * Return: number of nodes in the tree, 0 if tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+
+    return 1 + binary_tree_size(tree->left) + binary_tree_size(tree->right);
+}
+
+
+/**
  * heapify_down - Restores Max Heap property by moving a node down
  * @node: Pointer to the node to start bubbling down
  *
