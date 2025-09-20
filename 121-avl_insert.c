5d17e4c7d57e0136ec2a01b1f005cc60425eebe4
MmaGod1 <109483181+MmaGod1@users.noreply.github.com>
2025-09-20 12:36:44 +0100

    Update 121-avl_insert.c

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,8 +1,5 @@
 #include "binary_trees.h"
 
-/* Forward declaration */
-avl_t *avl_balance(avl_t *node);
-
 /**
  * avl_insert - Inserts a value in an AVL tree
  * @tree: Double pointer to the root node
@@ -12,64 +9,36 @@ avl_t *avl_balance(avl_t *node);
  */
 avl_t *avl_insert(avl_t **tree, int value)
 {
-	avl_t *node = NULL;
+    avl_t *node;
 
-	if (!tree)
-		return (NULL);
+    if (!tree)
+        return (NULL);
 
-	if (*tree == NULL)
-		return (*tree = binary_tree_node(NULL, value));
+    /* Insert as in a BST */
+    if (!*tree)
+        return (*tree = binary_tree_node(NULL, value));
 
-	if (value < (*tree)->n)
-		node = (*tree)->left ? avl_insert(&(*tree)->left, value) :
-			binary_tree_node(*tree, value);
-	else if (value > (*tree)->n)
-		node = (*tree)->right ? avl_insert(&(*tree)->right, value) :
-			binary_tree_node(*tree, value);
-	else
-		return (NULL); /* duplicate ignored */
+    if (value < (*tree)->n)
+        node = (*tree)->left ? avl_insert(&(*tree)->left, value)
+                              : (*tree)->left = binary_tree_node(*tree, value);
+    else if (value > (*tree)->n)
+        node = (*tree)->right ? avl_insert(&(*tree)->right, value)
+                               : (*tree)->right = binary_tree_node(*tree, value);
+    else
+        return (NULL); /* Ignore duplicates */
 
-	/* Balance this node */
-	*tree = avl_balance(*tree);
+    /* Rebalance if needed */
+    int balance = binary_tree_balance(*tree);
 
-	return (node);
-}
+    if (balance > 1)
+        *tree = (value < (*tree)->left->n) ? 
+                 binary_tree_rotate_right(*tree) : 
+                 (binary_tree_rotate_left((*tree)->left), binary_tree_rotate_right(*tree));
 
+    if (balance < -1)
+        *tree = (value > (*tree)->right->n) ? 
+                 binary_tree_rotate_left(*tree) : 
+                 (binary_tree_rotate_right((*tree)->right), binary_tree_rotate_left(*tree));
 
-#include "binary_trees.h"
-
-/**
- * avl_balance - Balances an AVL node if unbalanced
- * @node: Pointer to the node to balance
- *
- * Return: New root after rotation
- */
-avl_t *avl_balance(avl_t *node)
-{
-	int balance = binary_tree_balance(node);
-
-	/* Left heavy */
-	if (balance > 1)
-	{
-		if (binary_tree_balance(node->left) >= 0)
-			node = binary_tree_rotate_right(node);      /* Left-Left */
-		else
-		{
-			node->left = binary_tree_rotate_left(node->left); /* Left-Right */
-			node = binary_tree_rotate_right(node);
-		}
-	}
-	/* Right heavy */
-	else if (balance < -1)
-	{
-		if (binary_tree_balance(node->right) <= 0)
-			node = binary_tree_rotate_left(node);       /* Right-Right */
-		else
-		{
-			node->right = binary_tree_rotate_right(node->right); /* Right-Left */
-			node = binary_tree_rotate_left(node);
-		}
-	}
-
-	return (node);
+    return (node);
 }
